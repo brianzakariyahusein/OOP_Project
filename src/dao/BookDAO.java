9fa0a121797358d7635c0d56b1aa02500b994a9a
Brian Zakariya <zakariyabrian@gmail.com>
2026-02-27 17:32:32 +0700

    Create Data Access Object for Books

---
+++ src/dao/BookDAO.java
@@ -0,0 +1,39 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author Brian
+ */
+package dao;
+
+import config.DatabaseConnection;
+import model.Book;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class BookDAO {
+
+    public void insert(Book book) {
+        String sql = "INSERT INTO books (title, author,category, stock) VALUES (?,?,?,?)";
+
+        try (Connection conn = DatabaseConnection.getConnection(); PreparedStatement pst = conn.prepareStatement(sql)) {
+
+            pst.setString(1, book.getTitle());
+            pst.setString(2, book.getAuthor());
+            pst.setString(3, book.getCategory());
+            pst.setInt(4, book.getStock());
+
+            pst.executeUpdate();
+            System.out.println("Book inserted successfully!");
+
+        } catch (SQLException e) {
+            System.out.println("Insert failed: " + e.getMessage());
+        }
+    }
+
+}
