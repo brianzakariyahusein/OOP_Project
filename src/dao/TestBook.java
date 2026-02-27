d4a508d6a73e1a27c295db76dd9c05b0bb076ec1
Brian Zakariya <zakariyabrian@gmail.com>
2026-02-27 18:16:25 +0700

    Change the testBook DAO

--- src/dao/TestBook.java
+++ src/dao/TestBook.java
@@ -1,27 +1,22 @@
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Brian
- */
-
 package dao;
 
 import model.Book;
+import java.util.List;
 
 public class TestBook {
     public static void main(String[] args) {
-        Book book = new Book();
-        book.setTitle("OOP in JAVA");
-        book.setAuthor("Brian Zakariya");
-        book.setCategory("Programming");
-        book.setStock(10);
-        
+
         BookDAO dao = new BookDAO();
-        dao.insert(book);
-    } 
+        List<Book> books = dao.getAll();
+
+        for (Book b : books) {
+            System.out.println(
+                b.getId() + " - " +
+                b.getTitle() + " - " +
+                b.getAuthor() + " - " +
+                b.getCategory() + " - " +
+                b.getStock()
+            );
+        }
+    }
 }
