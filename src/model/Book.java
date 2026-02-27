8769ac66698b6ba8ba4a78ed5b851586144b38c0
Brian Zakariya <zakariyabrian@gmail.com>
2026-02-27 17:35:44 +0700

    update book model

---
+++ src/model/Book.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Brian
+ */
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private String category;
+    private int stock;
+    
+    public Book(){
+    }
+    
+    public Book(int id, String title,String author, String category, int stock) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.category = category;
+        this.stock = stock;
+    }
+    
+    public int getId(){
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
