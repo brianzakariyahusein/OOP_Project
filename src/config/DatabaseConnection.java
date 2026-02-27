3854a569cfe1d4171603de507822c19adf61b1c8
Brian Zakariya <zakariyabrian@gmail.com>
2026-02-27 17:04:10 +0700

    Configurasi database

---
+++ src/config/DatabaseConnection.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author Brian
+ */
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/library_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() {
+        try {
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connected to Database!");
+            return conn;
+        } catch (SQLException e) {
+            System.out.println("Connection Failed: " + e.getMessage());
+            return null;
+        }
+    }
+
+}
